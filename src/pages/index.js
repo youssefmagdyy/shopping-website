8ef6ca38d4d5caeedc82cfa71c77c29371f83229
youssefmagdyy <youssefmagdy1910@gmail.com>
2022-03-26 20:14:14 +0200

    updates

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import Cart from './CartPage'
+import About from './AboutPage'
+import PrivateRoute from './PrivateRoute'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import SingleProduct from './SingleProductPage'
+
+export {Home, Products, Cart, About, PrivateRoute, Error, Checkout,SingleProduct}
